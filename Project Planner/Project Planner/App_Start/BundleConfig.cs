c0c4302db57b0b1293b63920868ec419e970160b
Mikron Licud <leinmik.ml@gmail.com>
2015-10-29 19:46:39 +0000

    Structural changes to the scripts and CSS folders and the links to them.

--- Project Planner/Project Planner/App_Start/BundleConfig.cs
+++ Project Planner/Project Planner/App_Start/BundleConfig.cs
@@ -9,23 +9,18 @@ namespace Project_Planner
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jQuery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/jQuery/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap/bootstrap.js",
+                      "~/Scripts/respond/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap/bootstrap.css",
+                      "~/Content/custom-style.css"));
         }
     }
 }
