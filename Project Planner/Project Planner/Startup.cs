80da7becb903436a1141a97609f74d555ef7971c
Mikron Licud <leinmik.ml@gmail.com>
2015-10-29 19:07:08 +0000

    Initial Commit

---
+++ Project Planner/Project Planner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Planner.Startup))]
+namespace Project_Planner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
